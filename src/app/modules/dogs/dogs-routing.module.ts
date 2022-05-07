366abde8dc0cd37cf4721697f4bda4542ce668df
Edycar Reyes <edycarreyes@gmail.com>
2022-05-07 14:15:34 -0600

    linked the dogs-view withe it corresponded module

--- src/app/modules/dogs/dogs-routing.module.ts
+++ src/app/modules/dogs/dogs-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DogsViewComponent} from "./components/dogs-view/dogs-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DogsViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DogsRoutingModule { }
+export class DogsRoutingModule {
+}
