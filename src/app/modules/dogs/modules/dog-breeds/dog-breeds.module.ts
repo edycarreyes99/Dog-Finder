71298b43a341245938824298ea9a42731f1be73a
Edycar Reyes <edycarreyes@gmail.com>
2022-05-07 14:03:21 -0600

    generated the module for an specific breed of dog

---
+++ src/app/modules/dogs/modules/dog-breeds/dog-breeds.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DogBreedsRoutingModule } from './dog-breeds-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DogBreedsRoutingModule
+  ]
+})
+export class DogBreedsModule { }
