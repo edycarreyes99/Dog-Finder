aa869f12eed19fdd555b3e755556163cd40e8c5d
Edycar Reyes <edycarreyes@gmail.com>
2022-05-07 16:09:19 -0600

    designed the list row

--- src/app/modules/breeds/components/breeds-list/breeds-list.component.ts
+++ src/app/modules/breeds/components/breeds-list/breeds-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreedsService} from "../../services/breeds.service";
 
 @Component({
   selector: 'app-breeds-list',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreedsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly breedsService: BreedsService
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  async getBreeds(): Promise<object> {
+    return new Promise<object>(async (resolve, rejects) => {
+
+    });
+  }
 }
