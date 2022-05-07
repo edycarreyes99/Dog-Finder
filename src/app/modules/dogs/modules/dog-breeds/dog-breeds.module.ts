3bde8ea5105215e36f79dbf14f3df351c661eab7
Edycar Reyes <edycarreyes@gmail.com>
2022-05-07 15:05:33 -0600

    generated the component for the breed list feature

--- src/app/modules/dogs/modules/dog-breeds/dog-breeds.module.ts
+++ src/app/modules/dogs/modules/dog-breeds/dog-breeds.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DogBreedsRoutingModule } from './dog-breeds-routing.module';
+import { BreedsListComponent } from './components/breeds-list/breeds-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreedsListComponent
+  ],
   imports: [
     CommonModule,
     DogBreedsRoutingModule
